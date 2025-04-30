72101325d443787563c1347820d46bc0b4ee0b56
ta241003 <ngotheanh241003@gmail.com>
2025-05-01 01:02:38 +0700

    Registration/Login Logic Code (JWT, token storage)

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const authRouter = require('./routes/auth/auth-routes');
 
 mongoose
   .connect("mongodb+srv://cd4:Chuyende4@cluster0.n2zvkn0.mongodb.net/")
@@ -28,5 +29,6 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
