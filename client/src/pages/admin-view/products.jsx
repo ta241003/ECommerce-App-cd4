fb3c851ec83dd7bb82cc0d05658a36935c4851d2
nguyenvan1804 <nguyenhongvan1804@gmail.com>
2025-05-01 23:53:40 +0700

    Tao form tao san pham (Admin UI)

--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -1,40 +1,40 @@
-// import ProductImageUpload from "@/components/admin-view/image-upload";
-// import AdminProductTile from "@/components/admin-view/product-tile";
-// import CommonForm from "@/components/common/form";
-// import { Button } from "@/components/ui/button";
-// import {
-//     Sheet,
-//     SheetContent,
-//     SheetHeader,
-//     SheetTitle,
-// } from "@/components/ui/sheet";
-// import { useToast } from "@/components/ui/use-toast";
-// import { addProductFormElements } from "@/config";
-// import {
-//     addNewProduct,
-//     deleteProduct,
-//     editProduct,
-//     fetchAllProducts,
-// } from "@/store/admin/products-slice";
-// import { Fragment, useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
+import ProductImageUpload from "@/components/admin-view/image-upload";
+import AdminProductTile from "@/components/admin-view/product-tile";
+import CommonForm from "@/components/common/form";
+import { Button } from "@/components/ui/button";
+import {
+    Sheet,
+    SheetContent,
+    SheetHeader,
+    SheetTitle,
+} from "@/components/ui/sheet";
+import { useToast } from "@/components/ui/use-toast";
+import { addProductFormElements } from "@/config";
+import {
+    addNewProduct,
+    deleteProduct,
+    editProduct,
+    fetchAllProducts,
+} from "@/store/admin/products-slice";
+import { Fragment, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-// const initialFormData = {
-//     image: null,
-//     title: "",
-//     description: "",
-//     category: "",
-//     brand: "",
-//     price: "",
-//     salePrice: "",
-//     totalStock: "",
-//     averageReview: 0,
-// };
+const initialFormData = {
+    image: null,
+    title: "",
+    description: "",
+    category: "",
+    brand: "",
+    price: "",
+    salePrice: "",
+    totalStock: "",
+    averageReview: 0,
+};
 
 function AdminProducts() {
-    // const [openCreateProductsDialog, setOpenCreateProductsDialog] =
-    //     useState(false);
-    // const [formData, setFormData] = useState(initialFormData);
+    const [openCreateProductsDialog, setOpenCreateProductsDialog] =
+        useState(false);
+    const [formData, setFormData] = useState(initialFormData);
     // const [imageFile, setImageFile] = useState(null);
     // const [uploadedImageUrl, setUploadedImageUrl] = useState("");
     // const [imageLoadingState, setImageLoadingState] = useState(false);
@@ -44,8 +44,8 @@ function AdminProducts() {
     // const dispatch = useDispatch();
     // const { toast } = useToast();
 
-    // function onSubmit(event) {
-    //     event.preventDefault();
+    function onSubmit(event) {
+        event.preventDefault(); 
 
     //     currentEditedId !== null
     //         ? dispatch(
@@ -87,7 +87,7 @@ function AdminProducts() {
     //             dispatch(fetchAllProducts());
     //         }
     //     });
-    // }
+    }
 
     // function isFormValid() {
     //     return Object.keys(formData)
@@ -103,62 +103,61 @@ function AdminProducts() {
     // console.log(formData, "productList");
 
     return (
-        // <Fragment>
-        //     <div className="mb-5 w-full flex justify-end">
-        //         <Button onClick={() => setOpenCreateProductsDialog(true)}>
-        //             Add New Product
-        //         </Button>
-        //     </div>
-        //     <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4">
-        //         {productList && productList.length > 0
-        //             ? productList.map((productItem) => (
-        //                 <AdminProductTile
-        //                     setFormData={setFormData}
-        //                     setOpenCreateProductsDialog={setOpenCreateProductsDialog}
-        //                     setCurrentEditedId={setCurrentEditedId}
-        //                     product={productItem}
-        //                     handleDelete={handleDelete}
-        //                 />
-        //             ))
-        //             : null}
-        //     </div>
-        //     <Sheet
-        //         open={openCreateProductsDialog}
-        //         onOpenChange={() => {
-        //             setOpenCreateProductsDialog(false);
-        //             setCurrentEditedId(null);
-        //             setFormData(initialFormData);
-        //         }}
-        //     >
-        //         <SheetContent side="right" className="overflow-auto">
-        //             <SheetHeader>
-        //                 <SheetTitle>
-        //                     {currentEditedId !== null ? "Edit Product" : "Add New Product"}
-        //                 </SheetTitle>
-        //             </SheetHeader>
-        //             <ProductImageUpload
-        //                 imageFile={imageFile}
-        //                 setImageFile={setImageFile}
-        //                 uploadedImageUrl={uploadedImageUrl}
-        //                 setUploadedImageUrl={setUploadedImageUrl}
-        //                 setImageLoadingState={setImageLoadingState}
-        //                 imageLoadingState={imageLoadingState}
-        //                 isEditMode={currentEditedId !== null}
-        //             />
-        //             <div className="py-6">
-        //                 <CommonForm
-        //                     onSubmit={onSubmit}
-        //                     formData={formData}
-        //                     setFormData={setFormData}
-        //                     buttonText={currentEditedId !== null ? "Edit" : "Add"}
-        //                     formControls={addProductFormElements}
-        //                     isBtnDisabled={!isFormValid()}
-        //                 />
-        //             </div>
-        //         </SheetContent>
-        //     </Sheet>
-        // </Fragment>
-        <div>admin product</div>
+        <Fragment>
+            <div className="mb-5 w-full flex justify-end">
+                <Button onClick={() => setOpenCreateProductsDialog(true)}>
+                    Add New Product
+                </Button>
+            </div>
+            <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4">
+                {productList && productList.length > 0
+                    ? productList.map((productItem) => (
+                        <AdminProductTile
+                            setFormData={setFormData}
+                            setOpenCreateProductsDialog={setOpenCreateProductsDialog}
+                            setCurrentEditedId={setCurrentEditedId}
+                            product={productItem}
+                            handleDelete={handleDelete}
+                        />
+                    ))
+                    : null}
+            </div>
+            <Sheet
+                open={openCreateProductsDialog}
+                onOpenChange={() => {
+                    setOpenCreateProductsDialog(false);
+                    setCurrentEditedId(null);
+                    setFormData(initialFormData);
+                }}
+            >
+                <SheetContent side="right" className="overflow-auto">
+                    <SheetHeader>
+                        <SheetTitle>
+                            {currentEditedId !== null ? "Edit Product" : "Add New Product"}
+                        </SheetTitle>
+                    </SheetHeader>
+                    <ProductImageUpload
+                        imageFile={imageFile}
+                        setImageFile={setImageFile}
+                        uploadedImageUrl={uploadedImageUrl}
+                        setUploadedImageUrl={setUploadedImageUrl}
+                        setImageLoadingState={setImageLoadingState}
+                        imageLoadingState={imageLoadingState}
+                        isEditMode={currentEditedId !== null}
+                    />
+                    <div className="py-6">
+                        <CommonForm
+                            onSubmit={onSubmit}
+                            formData={formData}
+                            setFormData={setFormData}
+                            buttonText={currentEditedId !== null ? "Edit" : "Add"}
+                            formControls={addProductFormElements}
+                            isBtnDisabled={!isFormValid()}
+                        />
+                    </div>
+                </SheetContent>
+            </Sheet>
+        </Fragment>        
     );
 }
 
--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -7,128 +7,128 @@ const initialState = {
   user: null,
 };
 
-// export const registerUser = createAsyncThunk(
-//   "/auth/register",
+export const registerUser = createAsyncThunk(
+  "/auth/register",
 
-//   async (formData) => {
-//     const response = await axios.post(
-//       "http://localhost:5000/api/auth/register",
-//       formData,
-//       {
-//         withCredentials: true,
-//       }
-//     );
+  async (formData) => {
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/register",
+      formData,
+      {
+        withCredentials: true,
+      }
+    );
 
-//     return response.data;
-//   }
-// );
+    return response.data;
+  }
+);
 
-// export const loginUser = createAsyncThunk(
-//   "/auth/login",
+export const loginUser = createAsyncThunk(
+  "/auth/login",
 
-//   async (formData) => {
-//     const response = await axios.post(
-//       "http://localhost:5000/api/auth/login",
-//       formData,
-//       {
-//         withCredentials: true,
-//       }
-//     );
+  async (formData) => {
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/login",
+      formData,
+      {
+        withCredentials: true,
+      }
+    );
 
-//     return response.data;
-//   }
-// );
+    return response.data;
+  }
+);
 
-// export const logoutUser = createAsyncThunk(
-//   "/auth/logout",
+export const logoutUser = createAsyncThunk(
+  "/auth/logout",
 
-//   async () => {
-//     const response = await axios.post(
-//       "http://localhost:5000/api/auth/logout",
-//       {},
-//       {
-//         withCredentials: true,
-//       }
-//     );
+  async () => {
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/logout",
+      {},
+      {
+        withCredentials: true,
+      }
+    );
 
-//     return response.data;
-//   }
-// );
+    return response.data;
+  }
+);
 
-// export const checkAuth = createAsyncThunk(
-//   "/auth/checkauth",
+export const checkAuth = createAsyncThunk(
+  "/auth/checkauth",
 
-//   async () => {
-//     const response = await axios.get(
-//       "http://localhost:5000/api/auth/check-auth",
-//       {
-//         withCredentials: true,
-//         headers: {
-//           "Cache-Control":
-//             "no-store, no-cache, must-revalidate, proxy-revalidate",
-//         },
-//       }
-//     );
+  async () => {
+    const response = await axios.get(
+      "http://localhost:5000/api/auth/check-auth",
+      {
+        withCredentials: true,
+        headers: {
+          "Cache-Control":
+            "no-store, no-cache, must-revalidate, proxy-revalidate",
+        },
+      }
+    );
 
-//     return response.data;
-//   }
-// );
+    return response.data;
+  }
+);
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setUser: (state, action) => {},
+    setUser: (state, action) => { },
   },
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(registerUser.pending, (state) => {
-//         state.isLoading = true;
-//       })
-//       .addCase(registerUser.fulfilled, (state, action) => {
-//         state.isLoading = false;
-//         state.user = null;
-//         state.isAuthenticated = false;
-//       })
-//       .addCase(registerUser.rejected, (state, action) => {
-//         state.isLoading = false;
-//         state.user = null;
-//         state.isAuthenticated = false;
-//       })
-//       .addCase(loginUser.pending, (state) => {
-//         state.isLoading = true;
-//       })
-//       .addCase(loginUser.fulfilled, (state, action) => {
-//         console.log(action);
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        console.log(action);
 
-//         state.isLoading = false;
-//         state.user = action.payload.success ? action.payload.user : null;
-//         state.isAuthenticated = action.payload.success;
-//       })
-//       .addCase(loginUser.rejected, (state, action) => {
-//         state.isLoading = false;
-//         state.user = null;
-//         state.isAuthenticated = false;
-//       })
-//       .addCase(checkAuth.pending, (state) => {
-//         state.isLoading = true;
-//       })
-//       .addCase(checkAuth.fulfilled, (state, action) => {
-//         state.isLoading = false;
-//         state.user = action.payload.success ? action.payload.user : null;
-//         state.isAuthenticated = action.payload.success;
-//       })
-//       .addCase(checkAuth.rejected, (state, action) => {
-//         state.isLoading = false;
-//         state.user = null;
-//         state.isAuthenticated = false;
-//       })
-//       .addCase(logoutUser.fulfilled, (state, action) => {
-//         state.isLoading = false;
-//         state.user = null;
-//         state.isAuthenticated = false;
-//       });
-//   },
+        state.isLoading = false;
+        state.user = action.payload.success ? action.payload.user : null;
+        state.isAuthenticated = action.payload.success;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(checkAuth.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(checkAuth.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload.success ? action.payload.user : null;
+        state.isAuthenticated = action.payload.success;
+      })
+      .addCase(checkAuth.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(logoutUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      });
+  },
 });
 
 export const { setUser } = authSlice.actions;
