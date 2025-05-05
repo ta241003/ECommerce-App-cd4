f52595c51b17664c7c1cdc0692d9b438ab2c6309
nguyenvan1804 <nguyenhongvan1804@gmail.com>
2025-05-05 16:12:41 +0700

    create Cart + Add to Cart function

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -18,10 +18,10 @@ import {
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import { Avatar, AvatarFallback } from "../ui/avatar";
-// import { logoutUser } from "@/store/auth-slice";
-// import UserCartWrapper from "./cart-wrapper";
-// import { useEffect, useState } from "react";
-// import { fetchCartItems } from "@/store/shop/cart-slice";
+import { logoutUser } from "@/store/auth-slice";
+import UserCartWrapper from "./cart-wrapper";
+import { useEffect, useState } from "react";
+import { fetchCartItems } from "@/store/shop/cart-slice";
 import { Label } from "../ui/label";
 
 function MenuItems() {
