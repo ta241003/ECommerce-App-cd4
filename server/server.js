8b4a84651d3695c398093c3823e451b5d862cbfa
nguyenvan1804 <nguyenhongvan1804@gmail.com>
2025-05-10 15:12:46 +0700

    Create checkout page: add, edit, delete address

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const shopProductsRouter = require("./routes/shop/products-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
 const shopAddressRouter = require("./routes/shop/address-routes");
 const shopOrderRouter = require("./routes/shop/order-routes");
-// const shopSearchRouter = require("./routes/shop/search-routes");
+const shopSearchRouter = require("./routes/shop/search-routes");
 // const shopReviewRouter = require("./routes/shop/review-routes");
 
 // const commonFeatureRouter = require("./routes/common/feature-routes");
@@ -51,7 +51,7 @@ app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
-// app.use("/api/shop/search", shopSearchRouter);
+app.use("/api/shop/search", shopSearchRouter);
 // app.use("/api/shop/review", shopReviewRouter);
 
 // app.use("/api/common/feature", commonFeatureRouter);
