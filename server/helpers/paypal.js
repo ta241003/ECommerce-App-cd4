48f4d3e8fa083a38f4dcdcb9a4cddaedf1c3b0e8
ta241003 <ngotheanh241003@gmail.com>
2025-05-12 18:03:28 +0700

    Save order after payment function

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 const paypal = require('paypal-rest-sdk');
 
 paypal.configure({
-  mode: 'sandbox', // Change to 'live' when you move to production
-  client_id: 'Ad7y_AdXySLoQLIe9__f5yoX2J-CLiiL4ghhh062XuslpgOzjQpza9CoehDvMT-GMlM7WeEqEO5LA4xC',
-  client_secret: 'EMtWm7cqP4T-tCYvEO8_6W2EotcGUS_xYCNSLE_jeylZg5plFQffMPARf45opLjvYSeK7qGm3EFxp-a-'
+  mode: process.env.PAYPAL_MODE,
+  client_id: process.env.PAYPAL_CLIENT_ID,
+  client_secret: process.env.PAYPAL_CLIENT_SECRET,
 });
 
-module.exports = paypal
+module.exports = paypal;
